05a8badeb4d3607cfb181c974c9188aa5d86d011
YUBEENLIM <bgs0782@gmail.com>
2023-11-15 21:55:57 +0900

    14ê4강 calc19 까지 완료

---
+++ src/main/java/com/ll/practicesb20231115/PracticeSb20231115Application.java
@@ -0,0 +1,14 @@
+package com.ll.practicesb20231115;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeSb20231115Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracticeSb20231115Application.class, args);
+    }
+
+
+}
